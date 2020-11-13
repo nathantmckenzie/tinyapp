0e3bdbef5bfb10cdeb15c0e1e5305c89daf9f1e7
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-13 08:40:08 -0800

    refactoring and debugging

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require('bcrypt');
-const getUserByEmail = require("./helpers");
+const { getUserByEmail, urlsForUser, generateRandomString, users, urlDatabase } = require("./helpers");
 
 
 app.use(cookieSession({
@@ -16,69 +16,13 @@ app.set("view engine", "ejs");
 
 
 
-const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
-};
-
-
-const users = { 
-    "1": {
-      id: "1",
-      email: "user@example.com", 
-      password: "purple-monkey-dinosaur"
-    },
-    "2": {
-      id: "2",
-      email: "usesdfffar@example.com", 
-      password: "puasdfle-monkey-dinosaur"
-    }
-  }
-
-
-const genNextId = () => {
-  return Object.keys(users).length + 1;
-};
-
-const generateRandomString = () => {
-  const length = 6;
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var result = '';
-  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-  return result;
-}
-
-const emailCheck = function(emailInput) {
-  for (const keys in users) {
-    if (users[keys].email === emailInput) {
-      return true;
-    }
-  }
-  return false;
-}
-
-const urlsForUser = (id) => {
-  let obj = {};
-  for (const url in urlDatabase) {
-    if (urlDatabase[url].id === id) {
-      obj[url] = { longURL: urlDatabase[url].longURL, id: id };
-    }
-  }
-  return obj;
-};
-
-
-
 app.get("/urls", (req, res) => {
   const urls = urlsForUser(req.session["user_id"]);
   const templateVars = { user: users[req.session["user_id"]], urls: urls };
-  if (!req.session["user_id"]) {
-     res.redirect("/login")
-  }
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls_login", (req, res) => {
+app.get("/login", (req, res) => {
   const templateVars = { user: users[req.session["user_id"]], urls: urlDatabase };
   res.render('urls_login', templateVars);
 });
@@ -90,6 +34,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
+
   const templateVars = {user: users[req.session["user_id"]], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   if (!req.session["user_id"]) {
     res.redirect("/login");
@@ -112,9 +57,9 @@ app.get("/hello", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-    const templateVars = { user: users[req.session["user_id"]], urls: urlDatabase };
+    const templateVars = { user: users[req.session["user_id"]]};
     if (!users[req.session["user_id"]]); {
-      res.redirect('/login');
+      res.redirect('/urls');
     } 
     res.render("urls_new", templateVars);
 });
@@ -126,19 +71,21 @@ app.get('/login', (req, res) => {
 
 
 app.get("/register", (req, res) => {
-    const templateVars = { user: users[req.session["user_id"]], urls: urlDatabase };
+    const templateVars = { user: users[req.session["user_id"]]};
     res.render("register", templateVars);
 });
 
 
 
 app.post("/register", (req, res) => {
-  if (req.body.email === '' || req.body.password === '') {  //works
+  if (req.body.email === '' || req.body.password === '') { 
     res.status(400).send('Email Or Password Input Missing');
-  } else if (emailCheck(req.body.email)) { //doesn't work 
+  } else if (emailCheck(req.body.email)) { 
     res.status(400).send('Email Already Exists');
   } 
-
+    const genNextId = () => {
+      return Object.keys(users).length + 1;
+    };
     const id = genNextId();
     const email = req.body.email;
     const password = req.body.password;
@@ -146,7 +93,6 @@ app.post("/register", (req, res) => {
     const user = { id, email, password: hashedPassword };
     users[id] = user;
     
-    console.log(users);
     req.session['user_id'] = id;
     res.redirect('/urls');
 });
@@ -156,6 +102,7 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL].userID = req.session["user_id"];
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -182,8 +129,8 @@ app.post("/login", (req, res) => {
     }
 
     for (const id in users) {
-      if (emailCheck(email)) {
-        foundUser = users[id];
+      if (getUserByEmail(email, users) === id) {
+        res.status(400).send('Invalid Input');
       }
     }
 
@@ -192,7 +139,7 @@ app.post("/login", (req, res) => {
     }
 
     if (!bcrypt.compareSync(password, foundUser.password)) {
-      res.send('Incorrect Password - Try Again');
+      res.send('Invalid Input');
     }
   
     req.session["user_id"] = foundUser.id;
