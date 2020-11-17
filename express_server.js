bffdc6e35474e5bd1d7e6c2f301c9ad223c4fbd3
nathantmckenzie <54117589+nathantmckenzie@users.noreply.github.com>
2020-11-16 18:30:03 -0800

    Update express_server.js

--- express_server.js
+++ express_server.js
@@ -59,14 +59,13 @@ app.get("/urls/new", (req, res) => {
 
 //GET REQUEST TO VIEW THE WEBSITE OF THE SHORT URL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
 //GET REQUEST TO VIEW THE PAGE FOR EDITING THE SHORT URL
 app.get("/urls/:shortURL", (req, res) => {
   const databaseID = urlDatabase[req.params.shortURL]["id"];
-  console.log("what", urlDatabase, req.params.shortURL, urlDatabase[req.params.shortURL]);
   if (databaseID === req.session["user_id"]) {
     const templateVars = {
       user: users[req.session["user_id"]],
@@ -92,31 +91,22 @@ app.get("/register", (req, res) => {
 
 //POST REQUEST TO SUBMIT A FORM FOR NEW USER REGISTRATION
 app.post("/register", (req, res) => {
-  if (req.body.email === '' || req.body.password === '') {
-    res.status(400).send('Email Or Password Input Missing');
-  }
-
-  const genNextId = () => {
-    return Object.keys(users).length + 1;
-  };
-  const id = genNextId();
   const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  const user = {
-    id,
-    email,
-    password: hashedPassword
-  };
-  users[id] = user;
+  if (email === '' || password === '') {
+    return res.status(400).send('Enter Or Password Input Missing');
+  }
 
-  for (const key in users) {
-    if (getUserByEmail(email, user) === key) {
-      res.status(400).send("Invalid Input");
+  for (const user in users) {
+    if (getUserByEmail(email, users) === user) {
+      return res.status(400).send('Invalid Input');
     }
   }
 
-  req.session['user_id'] = id;
+  const newUserID = generateRandomString();
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  users[newUserID] = { id: newUserID, email, password: hashedPassword };
+  req.session["user_id"] = newUserID;
   res.redirect('/urls');
 });
 
